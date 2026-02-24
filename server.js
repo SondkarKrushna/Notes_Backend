9ee120f39997e5e2c67cd7c938bce4b733c7f265
Krushna Sondkar <sondkarkrushna@gmail.com>
2026-02-24 16:25:19 +0530

    cors access given

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ console.log("Mongo URI:", process.env.MONGO_URI);
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
